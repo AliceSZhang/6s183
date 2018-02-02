d5b260f6e76b7785eda498b339707f89542509b9
Yafarm <Yafarm@dhcp-18-111-25-248.dyn.mit.edu>
2018-02-02 13:23:51 -0500

    Initial Commit

---
+++ Maths Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maths Game
+//
+//  Created by Yafarm on 2/2/18.
+//  Copyright © 2018 Yafarm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
